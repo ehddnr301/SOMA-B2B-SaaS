69ccf7ccb3910aa12a80fb10b8231909f215d7b2
ehddnr301 <dy950328@gmail.com>
2025-03-08 01:04:36 +0000

    Refactor activity stream models: Remove duplicate and redundant model definitions

--- clickhouse.sh
+++ clickhouse.sh
@@ -6,6 +6,5 @@ docker run -d \
   -p 9009:9009 \
   -e CLICKHOUSE_USER=clickhouse \
   -e CLICKHOUSE_PASSWORD=clickhouse \
-  -e CLICKHOUSE_DB=clickhouse \
   -e CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT=1 \
   clickhouse/clickhouse-server
